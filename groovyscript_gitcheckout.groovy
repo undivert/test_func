1d0e1522440d7bf1297c088cbfcf24f7537b3d28
undivert <undivert@gmail.com>
2023-05-29 18:17:34 +0800

    update codes

--- groovyscript_gitcheckout.groovy
+++ groovyscript_gitcheckout.groovy
@@ -2,10 +2,17 @@ import hudson.plugins.git.GitSCM
 import hudson.plugins.git.GitTool
 import hudson.model.FreeStyleProject
 import hudson.model.FreeStyleBuild
+import jenkins.model.Jenkins
+import com.cloudbees.plugins.credentials.CredentialsProvider
+import com.cloudbees.plugins.credentials.domains.Domain
+import com.cloudbees.plugins.credentials.domains.DomainCredentials
 
 // Define the Git repository URL and branch
 def gitRepository = 'https://github.com/example/repository.git'
 def gitBranch = 'master'
+
+// Define the credentials ID configured in Jenkins
+def credentialsId = 'your-credentials-id'
 
 // Create a new FreeStyle project
 def project = new FreeStyleProject(Jenkins.instance, 'MyGitJob')
@@ -13,6 +20,12 @@ def project = new FreeStyleProject(Jenkins.instance, 'MyGitJob')
 // Define the Git SCM configuration
 def gitSCM = new GitSCM(gitRepository)
 gitSCM.setBranches([new hudson.plugins.git.BranchSpec(gitBranch)])
+
+// Get the credentials object
+def credentials = CredentialsProvider.findCredentialById(credentialsId, Domain.global())
+
+// Set the credentials to the Git SCM
+gitSCM.getUserRemoteConfigs()[0].credentialsId = credentials.getId()
 
 // Set the Git SCM to the project
 project.setScm(gitSCM)
