a8e1af846e33a6156eda5ce5e0bd499d3f271b2a
Tuan Pham <undivert@gmail.com>
2023-08-10 21:56:55 +0800

    Update modifyxml.groovy

--- modifyxml.groovy
+++ modifyxml.groovy
@@ -1,25 +1,24 @@
 import jenkins.model.Jenkins
 import hudson.XmlFile
+import hudson.model.FreeStyleProject  // Adjust the import based on your job type
 
 def jenkins = Jenkins.getInstance()
 
 def jobName = "myJob"  // Replace with the name of your job
 def job = jenkins.getItem(jobName)
 
-if (job != null) {
-    def configXml = job.getConfigFile().file
-    def xmlFile = new XmlFile(configXml)
-    
-    def xml = xmlFile.asString()
-    def xmlSlurper = new XmlSlurper().parseText(xml)
-    
-    // Remove the specific permission element
-    xmlSlurper.permissions.permission.find { it.text() == 'hudson.model.Item.Build:xxx' }.replaceNode {}
-    
-    // Save the modified XML
-    xmlFile.write(xmlSlurper)
-    
+if (job != null && job instanceof FreeStyleProject) {  // Adjust the job type accordingly
+    def configXml = job.getConfigFile()
+    def xmlFile = configXml.getFile()
+
+    // Read the current XML configuration
+    def xml = xmlFile.text
+    def updatedXml = xml.replaceAll(/<permission>hudson\.model\.Item\.Build:.*?<\/permission>/, '')
+
+    // Update the XML configuration
+    configXml.write(updatedXml)
+
     println("Job configuration modified successfully: $jobName")
 } else {
-    println("Job not found: $jobName")
+    println("Job not found or not the expected type: $jobName")
 }
